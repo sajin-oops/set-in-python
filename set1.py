28f397bfa63077d73acbd423c4389748104cf55d
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-24 13:05:58 +0530

    Add files via upload

---
+++ set1.py
@@ -0,0 +1,15 @@
+myset = {"apple","orange","banana"}
+
+print(myset)
+#O/P - {'apple', 'orange', 'banana'}
+
+# - True and 1 is considered the same value:
+
+myset = {"sajin","nijas",True,1,2}
+print(myset)
+
+#O/P - {True, 2, 'nijas', 'sajin'}
+print(len(myset))
+#O/P - 4
+print(type(myset))
+#O/P - <class 'set'>
